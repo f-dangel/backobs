d195cc52fe2045311154308e696a1de931f72126
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-02-10 22:47:06 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,39 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+# META
+##############################################################################
+AUTHORS = "F. Dangel"
+NAME = "deepobs-with-pytorch"
+PACKAGES = find_packages()
+
+DESCRIPTION = r"Make DeepOBS use BackPACK"
+LONG_DESCR = DESCRIPTION
+
+VERSION = "0.0.1"
+URL = "https://github.com/f-dangel/backpack-with-deepobs"
+LICENSE = "MIT"
+
+# DEPENDENCIES
+##############################################################################
+REQUIREMENTS_FILE = "requirements.txt"
+REQUIREMENTS_PATH = path.join(path.abspath(__file__), REQUIREMENTS_FILE)
+
+# with open(REQUIREMENTS_FILE) as f:
+#    requirements = f.read().splitlines()
+
+setup(
+    author=AUTHORS,
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCR,
+    long_description_content_type="text/markdown",
+    # install_requires=requirements,
+    url=URL,
+    license=LICENSE,
+    packages=PACKAGES,
+    zip_safe=False,
+    python_requires=">=3.5",
+)
