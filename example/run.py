bc6003409634fd3553fc7185cb52eea152b41324
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-02-15 17:53:59 +0100

    Pass check argument to SGD runner

--- example/run.py
+++ example/run.py
@@ -4,7 +4,7 @@ from runner import BackpackRunner
 from torch.optim import SGD
 
 
-def make_backpack_runner_for_sgd():
+def make_backpack_runner_for_sgd(check=True):
     """Create a BackpackRunner for the SGD optimizer."""
     optimizer_class_sgd = SGD
     hyperparams_sgd = {
@@ -12,7 +12,7 @@ def make_backpack_runner_for_sgd():
         "momentum": {"type": float, "default": 0.0,},
     }
 
-    return BackpackRunner(optimizer_class_sgd, hyperparams_sgd)
+    return BackpackRunner(optimizer_class_sgd, hyperparams_sgd, check_compatible=check)
 
 
 if __name__ == "__main__":
