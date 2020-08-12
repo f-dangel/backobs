8ad477baebe5cdca0d35a135a5f3afc93906ca32
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-08-12 09:49:05 +0200

    Add tests for individual gradients

--- test/test_extend_with_access_unreduced_loss.py
+++ test/test_extend_with_access_unreduced_loss.py
@@ -1,12 +1,13 @@
 """Accessing the unreduced loss should not change forward/backward."""
 
-from test.utils import check_sizes_and_values, set_up_problem
+from test.utils import (check_sizes_and_values, get_reduction_factor,
+                        set_up_problem)
 
 import pytest
 import torch
 
 from backobs.utils import SUPPORTED, UNSUPPORTED
-from backpack import backpack
+from backpack import backpack, extensions
 from deepobs.config import set_data_dir
 from deepobs.pytorch.config import set_default_device
 from deepobs.pytorch.testproblems import quadratic_deep
@@ -84,3 +85,48 @@ def test_same_backward(tproblem_cls, batch_size=3, seed=0):
         grad2 = [p.grad for p in tproblem2.net.parameters() if p.requires_grad]
 
     check_sizes_and_values(grad1, grad2)
+
+
+@pytest.mark.parametrize("tproblem_cls", SUPPORTED, ids=[p.__name__ for p in SUPPORTED])
+def test_same_batch_grad(tproblem_cls, batch_size=3, seed=0):
+    """Test individual gradients from unreduced losses match with BackPACK.
+
+    Args:
+        tproblem (TestProblem): DeepOBS test problem class.
+    """
+    # via backpack
+    tproblem1 = set_up_problem(
+        tproblem_cls, batch_size=batch_size, seed=seed, extend=True
+    )
+    loss1, acc1 = tproblem1.get_batch_loss_and_accuracy()
+    with backpack(extensions.BatchGrad()):
+        loss1.backward()
+
+    batch_grad1 = [p.grad_batch for p in tproblem1.net.parameters() if p.requires_grad]
+
+    # via autograd
+    tproblem2 = set_up_problem(
+        tproblem_cls, batch_size=batch_size, seed=seed, extend=True, unreduced_loss=True
+    )
+    loss2, acc2 = tproblem2.get_batch_loss_and_accuracy()
+    loss2_unreduced = loss2._unreduced_loss
+    factor = get_reduction_factor(loss2, loss2_unreduced)
+
+    # backpack assumes N individual losses, but MSELoss does not reduce non-batch
+    # axes, so we have to do it manually
+    if tproblem_cls == quadratic_deep:
+        loss2_unreduced = loss2_unreduced.flatten(start_dim=1).sum(1)
+
+    trainable_params = [p for p in tproblem2.net.parameters() if p.requires_grad]
+    batch_grad2 = [
+        torch.zeros(batch_size, *p.shape, device=p.device) for p in trainable_params
+    ]
+
+    for i in range(batch_size):
+        retain_graph = True if i < batch_size - 1 else False
+        l_i = loss2_unreduced[i]
+        grad = torch.autograd.grad(l_i, trainable_params, retain_graph=retain_graph)
+        for param_idx, g in enumerate(grad):
+            batch_grad2[param_idx][i] = g * factor
+
+    check_sizes_and_values(batch_grad1, batch_grad2)
